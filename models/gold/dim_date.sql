2de0e285f2960cb0d2202f656a06e9548f0c2f4e
Yakov Khanuk <your_real_email@gmail.com>
2026-04-15 22:30:46 -0400

    Update dim_date to match required structure

--- models/gold/dim_date.sql
+++ models/gold/dim_date.sql
@@ -1,18 +1,15 @@
 with distinct_dates as (
 
-    select distinct cast(order_date as date) as date_day
+    select distinct cast(order_date as date) as date
     from {{ ref('stg_orders') }}
 
 )
 
 select
-    cast(strftime(date_day, '%Y%m%d') as integer) as date_key,
-    date_day,
-    extract(year from date_day) as year_number,
-    extract(month from date_day) as month_number,
-    extract(day from date_day) as day_number,
-    strftime(date_day, '%Y-%m') as year_month,
-    strftime(date_day, '%B') as month_name,
-    strftime(date_day, '%w') as day_of_week_number,
-    strftime(date_day, '%A') as day_of_week_name
+    cast(strftime(date, '%Y%m%d') as integer) as DateKey,
+    date as Date,
+    extract(day from date) as Day,
+    extract(month from date) as Month,
+    extract(year from date) as Year,
+    extract(quarter from date) as Quarter
 from distinct_dates
